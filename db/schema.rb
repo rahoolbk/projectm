822fbc6dd234edbcaac694f9fd0a3136ed86f02b
komu_bhatt <rahool.kewalramani@gmail.com>
2016-02-11 16:43:12 +0530

    added cancancan and rolify

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211095143) do
+ActiveRecord::Schema.define(version: 20160211110511) do
 
   create_table "achievments", force: :cascade do |t|
     t.date     "dates"
